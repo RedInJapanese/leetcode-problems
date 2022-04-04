81953d2ed8e055996610a77de99da70d10a8ca41
Akash Basu <abasu924@gmail.com>
2022-04-04 10:06:22 -0700

    Update roman.py

--- roman.py
+++ roman.py
@@ -5,7 +5,21 @@ def romanToInt(s):
 
 
     while(i>0):
-        print("lol")
+        if(s[i-1] == 'I'):
+            sum+=1
+        if(s[i-1] == 'V'):
+            sum+=5
+        if(s[i-1] == 'X'):
+            sum+=10
+        if(s[i-1] == 'L'):
+            sum+=50
+        if(s[i-1] == 'C'):
+            sum+=100
+        if(s[i-1] == 'D'):
+            sum+=500
+        if(s[i-1] == 'M'):
+            sum+=1000
         i-=1
+    return sum
 
-romanToInt("heyheyhey")
+print("sum :",romanToInt("III"))
