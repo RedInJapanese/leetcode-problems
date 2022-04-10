e4d271fcc315ada7f700d7d1bce102a51f5f5acf
Akash Basu <abasu924@gmail.com>
2022-04-10 16:33:52 -0700

    Valid solution(with hash table)

--- twosum.py
+++ twosum.py
@@ -1,3 +1,6 @@
+import enum
+
+
 def twoSum(nums, target):
         """
         :type nums: List[int]
@@ -5,24 +8,26 @@ def twoSum(nums, target):
         :rtype: List[int]
         """
         weeder = []
-        lol = [] 
+        lol = []
+        d = {} 
         x = 1
         k = 0
         j = 0
+        check = 0
         for i in nums: 
-            if(i>target):
-                nums[j] = 0
+            if(i<=target):
+                d[i] = j
+                weeder.append(i)
             j+=1
-        
-        while(k<len(nums)):
-            while(x<len(nums)):
-                if(nums[x] == target - nums[k]):
-                    lol.append(k)
-                    lol.append(x)
+        while(k<len(weeder)):
+            while(x<len(weeder)):
+                if(weeder[x] == target - weeder[k]):
+                    lol.append(d[weeder[k]])
+                    lol.append(d[weeder[x]])
                     return lol
                 x+=1
             k+=1
             x = k+1
-nums = [2,0,11,9]
+nums = [22,0,11,9]
 output = twoSum(nums, 9)
 print(output)
