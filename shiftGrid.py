ce77d61f4b2839ac3ac2f496b6e7f84d8ef4723d
Akash Basu <abasu924@gmail.com>
2022-04-11 00:15:57 -0700

    Update shiftGrid.py

--- shiftGrid.py
+++ shiftGrid.py
@@ -8,38 +8,14 @@ def shiftGrid(grid, k):
     y = 2
     size = len(grid)
     check = []
-    #while(x<len(grid)):
-    #    vector = grid[x]
-    #    prev = grid[x][0]
-    #    vector[0]=grid[(x+size-k)%size][len(grid[x])-1]
-    #    vector[1]= prev
-    #    while(y<len(grid[x])):
-    #        #nex = grid[x][(y+k)%len(grid[x])]
-    #        y+=1
-    #        z+=1
-    #        #print(nex)
-    #    print(vector)
-    #    x+=1
-    #    y = 1
-    toroidal=grid[(0+size-k)%size][len(grid[0])-1]
-    for i in grid: 
+    for i in grid:
+        vector = i
+        vector[0]=grid[(x+size-k)%size][len(grid[x])-1]
         for j in i:
-            prev = grid[x][(z+len(i)-1)%len(i)]
-            check.append(prev)
-            z+=1
-        #print(check)
-        grid[x] = check
-        check = []
+            y+=1
+            z+=1 
         x+=1
-        z = 0
-    for y in grid: 
-        toroidal=grid[(z+size-k)%size][0]
-        check.append(toroidal)
-        z+=1
-    z = 0
-    for p in grid: 
-        grid[z][0] = check[z]
-        z+=1
+        y = 1
     return grid
 grid = [[1,2,3],[4,5,6],[7,8,9]]
 k = 1
