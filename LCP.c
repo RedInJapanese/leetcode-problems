dbf575d8f28b78b668bbc7e43a1d46e531381586
RedInJapanese <abasu924@gmail.com>
2022-04-12 08:47:32 +0000

    Test system()

--- LCP.c
+++ LCP.c
@@ -1,12 +1,15 @@
 #include <string.h>
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h> 
+//#include <errno.h>
 /*char *longestCommonPrefix(char ** strs, int strsSize);
 
 char *longestCommonPrefix(char ** strs, int strsSize){
     
 }*/
 int main(void) {
-    puts("test");
+	system("ls");
     return 0;
 }
