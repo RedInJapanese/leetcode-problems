0ad7652fd9b763f18f363a454986985347f95cd1
Akash Basu <abasu924@gmail.com>
2022-04-12 00:58:14 -0700

    Create LCP.c

---
+++ LCP.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdio.h>
+
+char *longestCommonPrefix(char ** strs, int strsSize);
+
+char *longestCommonPrefix(char ** strs, int strsSize){
+    
+}
+int main(void) {
+    return 0;
+}
