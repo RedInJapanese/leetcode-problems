2ceb237d95e3eff7adb25edcca7ff4a6eeaaf916
Akash Basu <abasu924@gmail.com>
2022-04-13 10:01:15 -0700

    Update SpiralMatrix.c

--- SpiralMatrix.c
+++ SpiralMatrix.c
@@ -9,10 +9,17 @@ void generateMatrix(int n, int* returnSize, int** returnColumnSizes){
     returnSize = (int*)malloc((n*n)*sizeof(int));
     returnColumnSizes = (int**)malloc(n*sizeof(int*));
     int x = 1;
+    int y = 0;
     for(int i = 0;i <(n*n); i++) {
         returnSize[i] = x;
-        printf("%d ", returnSize[i]);
-	x++;
+        x++;
+    }
+    for(int j = 0; j<n; j++) {
+        for(int k = 0; k<n; k++) {
+            returnColumnSizes[j][k] = returnSize[y];
+            printf("%d ", returnColumnSizes[j][k]);
+            y++;
+        }
     }
     printf("\n");
 
