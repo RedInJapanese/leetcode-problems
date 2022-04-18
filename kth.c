6954d8e77378b1981589861ab3b1b04cf26d4bb8
Akash Basu <abasu924@gmail.com>
2022-04-17 18:03:29 -0700

    Create kth.c

---
+++ kth.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+ struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+ };
+
+
+int kthSmallest(struct TreeNode* root, int k){
+    
+}
+
+int main(void) {
+    puts("test"); 
+    return 0;
+}
