8215c717015467aa4f3ba68c939a2c8d89e30fd1
Akash Basu <abasu924@gmail.com>
2022-05-01 01:04:43 -0700

    valid solution

--- search.c
+++ search.c
@@ -5,14 +5,14 @@ int search(int* nums, int numsSize, int target){
     int right = numsSize-1;
     while(left<=right) {
         int mid = left+(right-left)/2;
-        if(nums[mid] == target) {
-            return mid;
-        }
         if(nums[mid]>target) {
             right = mid-1;
         }
-        else {
+        else if(nums[mid]< target) {
             left = mid+1;
+        }
+        else {
+            return mid;
         }
     }
     return -1;
