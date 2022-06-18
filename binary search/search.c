8f717fec9bc3a5e4b8420896c697ea3bf6c9cd12
Akash Basu <abasu924@gmail.com>
2022-06-18 09:51:21 -0700

    organize files

---
+++ binary search/search.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <stdint.h> 
+#include <inttypes.h> 
+
+int search(int* nums, int numsSize, int target);
+
+int main(void) {
+    puts("any lmaoers?");
+    return 0;
+}
+
+int search(int* nums, int numsSize, int target) {
+    
+}
