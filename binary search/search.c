f03e36926c404b9ea90cd543c35b9722803c2503
Akash Basu <abasu924@gmail.com>
2022-06-18 15:37:10 -0700

    Update search.c

--- binary search/search.c
+++ binary search/search.c
@@ -14,7 +14,26 @@ int main(void) {
 }
 
 int search(int* nums, int numsSize, int target) {
-       
+    int pivot, L, R;
+    L = pivot = 0; R = numsSize-1;
+    if(numsSize == 1 && nums[0] == target) {return 0;}
+    if(numsSize == 2 && nums[0] == target) {return 0;}
+    if(numsSize == 2 && nums[1] == target) {return 1;}
+    if(nums[L] == target) {return L;}
+    if(nums[R] == target){return R;}
+    while(L<=R) {
+        pivot = L +(R-L)/2;
+        if(nums[pivot] == target) {
+            return pivot;
+        }
+        if(target < nums[pivot]) {
+            R = pivot-1;
+        }
+        else {
+            L = pivot+1;
+        }
+    }
+    return -1;
 }
 
 int searchEasy(int *nums, int numsSize, int target) {
