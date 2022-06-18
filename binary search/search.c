8ca1e3383268ed976e43c50e5d23a93227f594d2
Akash Basu <abasu924@gmail.com>
2022-06-18 13:18:14 -0700

    Update search.c

--- binary search/search.c
+++ binary search/search.c
@@ -5,6 +5,8 @@
 #include <inttypes.h> 
 
 int search(int* nums, int numsSize, int target);
+int searchEasy(int* nums, int numsSize, int target);
+
 
 int main(void) {
     puts("any lmaoers?");
@@ -12,5 +14,15 @@ int main(void) {
 }
 
 int search(int* nums, int numsSize, int target) {
-    
+       
+}
+
+int searchEasy(int *nums, int numsSize, int target) {
+    if(numsSize == 1 && nums[0]== target){return 0;}
+    for(int i = 0; i<numsSize; ++i) {
+        if(nums[i] == target) {
+            return i;
+        }
+    }
+    return -1;
 }
