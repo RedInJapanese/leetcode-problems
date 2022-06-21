e30ec65283f364ae29d445e743135c9c38c4c820
Akash Basu <abasu924@gmail.com>
2022-06-20 17:25:56 -0700

    Update leet167.py

--- leet167.py
+++ leet167.py
@@ -1,20 +1,15 @@
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        lol = []
-        d = {} 
-        k = 0
-        f = 0
-        for i in numbers:
-            d[i]=f
-            f+=1
-        for x in numbers: 
-            if target-x in d:
-                if(k != d[target-x]):
-                    lol.append(k+1)
-                    lol.append((d[target-x])+1)
-                    return lol
+def twoSum(numbers, target):
+    lol = []
+    d = {} 
+    k = 0
+    f = 0
+    for i in numbers:
+        d[i]=f
+        f+=1
+    for x in numbers: 
+        if target-x in d:
+            if(k != d[target-x]):
+                lol.append(k+1)
+                lol.append((d[target-x])+1)
+                return lol
             k+=1
