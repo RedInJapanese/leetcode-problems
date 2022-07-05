a4d76ae003bdfee1912a1659f9e0fadb7ba17e43
Akash Basu <abasu924@gmail.com>
2022-07-05 11:02:52 -0700

    Create leet125.py

---
+++ Two pointers/leet125.py
@@ -0,0 +1,17 @@
+def isPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    s = re.sub(r"[^a-zA-Z0-9]","",s)
+    s = s.lower()
+        
+    i = 0
+    j = len(s)-1
+        
+    while(i<j):
+        if s[i] != s[j]:
+            return False
+        i+=1
+        j-=1
+    return True
